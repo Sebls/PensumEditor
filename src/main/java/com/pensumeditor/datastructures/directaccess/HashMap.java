de8d2f6136cfc6ca42976c1d33bf8938c82768fc
Sebastian Lopez <sebas.lopezsilva147@gmail.com>
2023-06-25 13:11:09 -0500

    Agregadas Universal Hashing para String y Enteros en la clase HashMap

--- src/main/java/com/pensumeditor/datastructures/directaccess/HashMap.java
+++ src/main/java/com/pensumeditor/datastructures/directaccess/HashMap.java
@@ -5,74 +5,101 @@ import com.pensumeditor.datastructures.exceptions.KeyNotFoundException;
 import com.pensumeditor.datastructures.linear.LinkedList;
 
 import java.util.Objects;
+import java.util.Random;
 
-public class HashMap<K,V>{
-    public class MapNode {
+public class HashMap<K, V>{
+
+    private class MapNode {
         private K key;
         private V value;
+
         public MapNode(K key, V value) {
             this.key = key;
             this.value = value;
         }
+
         public K getKey() {
             return key;
         }
-
         public V getValue() {
             return value;
         }
-    }
-    private int numElements;
-    private int size;
-    private boolean loadFactor() {
-        return (numElements/size) > 0.9;
-    }
-
-    private LinkedList<MapNode>[] hashTable;
-    public HashMap(int size) {
-        this.size = size;
-        hashTable = new LinkedList[size];
-    }
-
-    private boolean containsKey(K key) {
-        int index = hash(key,size);
-        if (Objects.nonNull(hashTable[index]) && hashTable[index].getSize() > 0) {
-            for (int i=0; i<size; i++) {
-                if (hashTable[index].get(i).getKey().equals(key)) {
-                    return true;
-                }
-            }
+        public void setKey(K key) {
+            this.key = key;
         }
-        return false;
+        public void setValue(V value) {
+            this.value = value;
+        }
     }
 
-    private int hash(K key,int m) {
+    // HashTable attributes
+    private LinkedList<MapNode>[] hashTable;
+    private int size;
+    private int capacity;
+    private double loadFactor;
+
+    //HashFunction for integers parameters
+    private final Random random;
+    private int a, b;
+    private int p;
+
+    public HashMap() {
+        this(16, 0.9);
+    }
+
+    public HashMap(int capacity) {
+        this(capacity, 0.9);
+    }
+
+    public HashMap(int capacity, double loadFactor) {
+        random = new Random();
+        this.p = 10000019;
+        this.a = random.nextInt(1, p-1);
+        this.b = random.nextInt(0, p-1);
+        this.capacity = capacity;
+        this.loadFactor = loadFactor;
+        hashTable = new LinkedList[capacity];
+    }
+
+
+    private int hash(K key, int m) {
         if (key instanceof String value) {
-            int hashed = 0;
-            for (int i = 0; i < value.length(); i++) {
-                hashed += value.charAt(i);
-            }
-            return hashed % m;
+            return modFunction(PolyHash(value), capacity);
         } else if (key instanceof Integer value){
-            return value % m;
+            return modFunction(modFunction(a*value+b, p), m);
         } else {
             throw new TypeOfKeyStillNotSupportedException();
         }
     }
 
+    public int PolyHash(String s) {
+        int hash = 0;
+        for (int i = s.length()-1; i >= 0 ; i --) {
+            hash = modFunction(hash * 31 + s.charAt(i), p);
+        }
+        return hash;
+    }
+
+    public int modFunction(int a, int mod) {
+        return (a % mod + mod) % mod;
+    }
+
     public void put(K key, V value) {
-        int index = hash(key,size);
+        if (checkLoadFactor()) {
+            rehash();
+        }
+        int index = hash(key, capacity);
         if (Objects.isNull(hashTable[index])) {
             hashTable[index] = new LinkedList<MapNode>();
         }
         hashTable[index].add(new MapNode(key,value));
-        numElements++;
+        size++;
     }
 
     public V get(K key) {
-        if (containsKey(key)) {
-            int index = hash(key,size);
-            for (int i=0; i<size; i++) {
+        int index = hash(key, capacity);
+        if (Objects.nonNull(hashTable[index]) && hashTable[index].getSize() > 0) {
+            for (int i = 0; i< capacity; i++) {
                 if (hashTable[index].get(i).getKey().equals(key)) {
                     return hashTable[index].get(i).getValue();
                 }
@@ -81,26 +108,44 @@ public class HashMap<K,V>{
         throw new KeyNotFoundException();
     }
 
-    public void remove(K key) {
-        if (containsKey(key)) {
-            int index = hash(key,size);
-            for (int i=0; i<size; i++) {
+    public V remove(K key) {
+        int index = hash(key, capacity);
+        if (Objects.nonNull(hashTable[index]) && hashTable[index].getSize() > 0) {
+            for (int i = 0; i < capacity; i++) {
                 if (hashTable[index].get(i).getKey().equals(key)) {
-                    hashTable[index].remove(i);
+                    return hashTable[index].remove(i).getValue();
                 }
             }
         }
         throw new KeyNotFoundException();
     }
 
+    private boolean containsKey(K key) {
+        int index = hash(key, capacity);
+        if (Objects.nonNull(hashTable[index]) && hashTable[index].getSize() > 0) {
+            for (int i = 0; i < capacity; i++) {
+                if (hashTable[index].get(i).getKey().equals(key)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean checkLoadFactor() {
+        return ((double) size / capacity) > loadFactor;
+    }
+
     private void rehash(){
-        LinkedList<MapNode>[] newTable = new LinkedList[size*2];
+        LinkedList<MapNode>[] newTable = new LinkedList[capacity * 2];
+        this.a = random.nextInt(1, p-1);
+        this.b = random.nextInt(0, p-1);
         for (int i = 0;i < hashTable.length;i++){
              if (Objects.nonNull(hashTable[i])) {
-                 for (int j = 0;j < hashTable[i].getSize();j++){
+                 for (int j = 0; j < hashTable[i].getSize(); j++){
                      K key = hashTable[i].get(j).getKey();
                      V value = hashTable[i].get(j).getValue();
-                     int index = hash(key,size*2);
+                     int index = hash(key, capacity * 2);
                      if (Objects.isNull(newTable[index])) {
                          newTable[index] = new LinkedList<MapNode>();
                      }
@@ -108,7 +153,7 @@ public class HashMap<K,V>{
                  }
              }
         }
-        size *= 2;
+        capacity *= 2;
         hashTable = newTable;
     }
 }
--- src/main/java/com/pensumeditor/gui/Test.java
+++ src/main/java/com/pensumeditor/gui/Test.java
@@ -6,13 +6,22 @@ import com.pensumeditor.datastructures.trees.AVLTree;
 
 public class Test {
     public static void main(String[] args) {
-        HashMap<Integer,String> hash = new HashMap<>(16);
-        hash.put(543,"mate");
-        hash.put(559,"arqui");
-        System.out.println(hash.get(543));
-        System.out.println(hash.get(559));
-        hash.remove(543);
-        System.out.println(hash.get(543));
+        HashMap<Integer,String> hash = new HashMap<>(4);
+        hash.put(2016722,"Matematicas");
+        hash.put(1016735,"Arquitectura");
+        hash.put(6216723,"Probabilidad");
+        hash.put(7026738,"Algebra");
+        hash.put(8286719,"Mecanica");
+        hash.put(4086730,"Electricidad");
+        System.out.println(hash.get(2016722));
+        System.out.println(hash.get(1016735));
+        System.out.println(hash.get(6216723));
+        System.out.println(hash.get(7026738));
+        System.out.println(hash.get(8286719));
+        System.out.println(hash.get(4086730));
+
+        /*hash.put("abcd", 304354);
+        System.out.println(hash.get("abcd"));*/
 
     }
 }
